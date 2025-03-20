126c1aedb5b8fbb16754d830e83b8b89bef5ef5e
CleverDarkSolutions <RotmgADAS@o2.pl>
2025-03-20 21:13:58 +0100

    Routing improved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {AboutUsComponent} from './pages/about-us/about-us.component';
 import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
 import {RentalsComponent} from './pages/rentals/rentals.component';
 import {EventsComponent} from './pages/events/events.component';
+import {SaleComponent} from './pages/sale/sale.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
   { path: 'about', component: AboutUsComponent},
   { path: 'rentals', component: RentalsComponent },
   { path: 'events', component: EventsComponent },
+  { path: 'sale', component: SaleComponent },
   { path: '**', component: PageNotFoundComponent } // Wildcard for unknown paths
 ];
